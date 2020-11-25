a5c8abe9a15a4419f4f330d7db305f3992acbaf2
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2020-11-25 16:21:52 +0530

    added gatsby-filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,27 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
 }
